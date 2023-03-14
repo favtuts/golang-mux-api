735af35d5c3064b0679b7ae4b567f3d9d0618c46
favtuts <favtuts@gmail.com>
2023-03-14 16:57:43 +0700

    create Post REST APIs

--- server.go
+++ server.go
@@ -11,9 +11,11 @@ import (
 func main() {
 	router := mux.NewRouter()
 	const port string = ":8000"
-	router.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+	router.HandleFunc("/", func(resp http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(resp, "Up and running...")
 	})
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", addPost).Methods("POST")
 	log.Println("Server listening on port", port)
 	log.Fatalln(http.ListenAndServe(port, router))
 }
